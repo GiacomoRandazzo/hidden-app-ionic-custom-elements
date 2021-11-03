bd383d17640c1a5d30775be3a53da2baad5d9600
Giacomo Randazzo <giacomoran@gmail.com>
2021-11-03 23:00:13 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import { initialize } from "@ionic/core/components";
+import { IonBadge } from "@ionic/core/components/ion-badge";
+import "./app.css";
+
+initialize();
+
+customElements.define("ion-badge", IonBadge);
